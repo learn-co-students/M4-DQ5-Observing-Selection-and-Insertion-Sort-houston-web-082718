35f997123c8bc35660cabe617a6173797567af16
V10LET <violet.wmoon@gmail.com>
2018-11-05 13:38:16 -0600

    sorts

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (var i = 1; i < arr.length; i++) {
+      j = i
+      while (j > 0 && arr[j-1] > arr[j]) {
+          swap(arr, j, j-1)
+          j -= 1
+      }
+  }
+}
+
+function swap(arr, num1, num2) {
+    let x = arr[num1]
+    let y = arr[num2]
+    arr[num2] = x
+    arr[num1] = y
 }
